87016ee9c1c0fd94c37a474b3374a1d490fb0406
enwudz <77306741+enwudz@users.noreply.github.com>
2023-10-17 13:35:03 -0400

    update runclip
    update to run all

--- runClip.py
+++ runClip.py
@@ -22,14 +22,14 @@ def main(movie_file):
     # <=== put a # before a line to comment it off
     
     ## make an excel file for a clip
-    # initializeClip.main(movie_file)
+    initializeClip.main(movie_file)
     
     ## automated path tracking, including speed, turns, stops
     # if tracking wonky at 12, try 25 or 40 or ...  eg: (movie_file, 25, True) 
     autoTracker.main(movie_file, 12, True) # True is show tracking
     analyzeTrack.main(movie_file) # when finished with autoTracker
 
-    plotClip.main(movie_file)
+    # plotClip.main(movie_file)
     # rotaZoomer.main(movie_file)
     
     ## step-by-step timing
