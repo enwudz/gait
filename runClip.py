4d5a10756bb360f9f27df7bc4407e473caa56b19
enwudz <77306741+enwudz@users.noreply.github.com>
2024-01-19 09:09:42 -0500

    updates from lab

--- runClip.py
+++ runClip.py
@@ -28,13 +28,13 @@ def main(movie_file):
     ## automated path tracking, including speed, turns, stops
     # if tracking wonky at 12, try 25 or 40 or ...  eg: (movie_file, 25, True) 
     #autoTracker.main(movie_file, 25, True) # True is show tracking
-    #analyzeTrack.main(movie_file) # when finished with autoTracker
+    analyzeTrack.main(movie_file) # when finished with autoTracker
 
     # show demo track
     #demoTracking.main(movie_file, False)
 
     ## show plots
-    plotClip.main(movie_file, 'track')
+    #plotClip.main(movie_file, 'track')
 
     ## step-by-step timing
     # frameStepper.main(movie_file, 100)
